3cb6d4b53b0ed97a83888aee7bde6f0e8037e66d
aneeshs005 <aneesh@ebrainchild.services>
2020-09-09 00:07:59 +0530

    Changed Folder structure, added programs to cs

---
+++ C#/cs01/06.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Brainchild.Curriculum.Solutions
+{
+    public class Solution01
+    {
+        public bool ToDo(int a, floar b,string c)
+        {
+            throw new NotImplementedException("Please write your solution here");
+        }
+    }
+}
---
+++ C#/cs01/tests/02.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+namespace Brainchild.Curriculum
+{
+    [TestFixture]
+    public class Test02
+    {
+        [SetUp]
+        public void SetUp()
+        {
+        }
+        [Test]
+        public void Test01()
+        {
+            Assert.Fail();
+        }
+      }
+}
