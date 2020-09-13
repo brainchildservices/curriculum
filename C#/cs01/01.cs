900f6f46f18d0448e1f13bedcfcbd3dc40748031
aneeshs005 <aneesh@ebrainchild.services>
2020-09-13 23:14:37 +0530

    Solution 1 to 4 and tests completed

--- C#/cs01/01.cs
+++ C#/cs01/01.cs
@@ -1,11 +1,6 @@
 using System;
 namespace Brainchild.Curriculum.Solutions
 {
-<<<<<<< HEAD
-    public class FizzBuzz
-    {
-        public string PrintIndividual(int a)
-=======
     /*
     Write a program that prints the numbers from 1 to 100 and for multiples of '3' print "Fizz" instead of the number 
     and for the multiples of '5' print "Buzz".
@@ -17,7 +12,6 @@ namespace Brainchild.Curriculum.Solutions
     {
         ///Accept one number and return a string(Fizz or Buzz or the number ) based on the aforementioned logic 
         public string Print(int a)
->>>>>>> aneeshSub
         {
             if(a%3==0 && a%5==0){
                 return "Fizz Buzz";
@@ -32,14 +26,9 @@ namespace Brainchild.Curriculum.Solutions
                 return a.ToString();
             }
         }
-<<<<<<< HEAD
-        public string[] PrintNumbers(int length)
-        {
-=======
 
         ///Accept one number and return an array of strings(Fizz or Buzz or the number ) for each value 0 to num passed to the below function
         public string[] PrintNumbers(int length){
->>>>>>> aneeshSub
             string[] result=new string[length];
             for (int i = 1; i <=length; i++)
             {
--- C#/cs01/tests/01.cs
+++ C#/cs01/tests/01.cs
@@ -11,57 +11,12 @@ namespace Brainchild.Curriculum
         }
         
         [Test]
-<<<<<<< HEAD
-        public void TestNumberOtherThan3or5Divisible()
-        {
-            FizzBuzz printer=new FizzBuzz();
-            string result=printer.PrintIndividual(2);
-            Assert.AreEqual(result,"2");
-        }
-        [Test]
-        public void TestNumberDivisibleBy3()
-        {
-            FizzBuzz printer=new FizzBuzz();
-            string result=printer.PrintIndividual(6);
-            Assert.AreEqual(result,"Fizz");
-        }
-        [Test]
-        public void TestNumberDivisibleBy5()
-        {
-            FizzBuzz printer=new FizzBuzz();
-            string result=printer.PrintIndividual(10);
-            Assert.AreEqual(result,"Buzz");
-        }
-        [Test]
-        public void TestNumberDivisibleBy3and5()
-        {
-            FizzBuzz printer=new FizzBuzz();
-            string result=printer.PrintIndividual(15);
-            Assert.AreEqual(result,"Fizz Buzz");
-        }
-        [Test]
-        public void TestPrintFizzBuzzTo3()
-        {
-            FizzBuzz printer=new FizzBuzz();
-            string[] result=printer.PrintNumbers(3);
-            string[] myString=new string[3]{"1","2","Fizz"};
-            Assert.AreEqual(result,myString);
-        }
-        [Test]
-        public void TestPrintFizzBuzzTo15()
-        {
-            FizzBuzz printer=new FizzBuzz();
-            string[] result=printer.PrintNumbers(15);
-            string[] myString=new string[15]{"1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","Fizz Buzz"};
-            Assert.AreEqual(result,myString);
-=======
         //write all test cases which covers FizzBuzzPrinter Print and PrintNumbers logics
         public void TestNumberOtherThan3or5Divisible()
         {
             FizzBuzzPrinter printer=new FizzBuzzPrinter();
             string result=printer.Print(2);
             Assert.AreEqual(result,"2");
->>>>>>> aneeshSub
         }
         [Test]
         public void TestNumberDivisibleBy3()
