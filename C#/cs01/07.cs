d76bd1339591d140a84d71a4f701bfd1db828894
Rohith Rajan <rohithkrajan@gmail.com>
2020-09-15 07:35:54 +0530

    Update 07.cs

--- C#/cs01/07.cs
+++ C#/cs01/07.cs
@@ -1,6 +1,20 @@
 using System;
 namespace Brainchild.Curriculum.Solutions
 {
+/*
+Create a Fact method that receives a non-negative integer and returns the factorial of that number.
+
+Examples
+Fact(1) ➞ 1
+
+Fact(3) ➞ 6
+
+Fact(6) ➞ 720
+Notes
+Consider that 0! = 1.
+You should return a long data type number.
+use recursion
+*/
     public class Solution07
     {
         public bool ToDo(int a, float b,string c)
