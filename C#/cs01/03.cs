9fe51818cbd480d0a761b219bcf63cf78a58e7a6
sreeragps <sreerag@brainchild.services>
2020-09-20 06:52:49 +0530

    Made changes to program 3 and test completed.

--- C#/cs01/03.cs
+++ C#/cs01/03.cs
@@ -2,25 +2,24 @@ using System;
 namespace Brainchild.Curriculum.Solutions
 {
     //implement bubblesort in C# and also create the necessary tests
-    public class BubbleSort
+    public class BubbleSorting
     {
         public int[] Sort(int[] array){
             int length=array.Length;
             for (int i = 0; i < length; i++)
             {
-                bool swap=false;
-                for (int j = 0; j < length-1; j++)
-                {
-                    if(array[j]>array[j+1]){
-                        int temp=array[j];
-                        array[j]=array[j+1];
-                        array[j+1]=temp;
-                        swap=true;
+               
+                int j= 0;
+                 do {
+                    if( array [ j ]>array[ j + 1 ]) {
+                        int x = array [ j ];
+                        array [ j ] = array[ j + 1 ];
+                        array [ j + 1 ] = x;
+                       
                     }
-                }
-                if(swap==false){
-                    break;
-                }
+                    j++;
+                }while( j < length-1 );
+              
             }
             return array;
         }
--- C#/cs01/tests/03.cs
+++ C#/cs01/tests/03.cs
@@ -12,20 +12,20 @@ namespace Brainchild.Curriculum
          [Test]
         public void BubbleSortArrayLength4()
         {
-            BubbleSort testSort= new BubbleSort();
-            int[] myArray=new int[]{7,3,88,2};
-            int[] testSorted=testSort.Sort(myArray);
-            int[] result=new int[]{2,3,7,88};
-            Assert.AreEqual(result,testSorted);
+            BubbleSorting bubblesort= new BubbleSorting();
+            int[] arraytobesorted=new int[]{7,3,88,2};
+            int[] sortedArray=bubblesort.Sort(arraytobesorted);
+            int[] expectedArray=new int[]{2,3,7,88};
+            Assert.AreEqual(expectedArray,sortedArray);
         }
         [Test]
         public void BubbleSortArrayLength7()
         {
-            BubbleSort test= new BubbleSort();
-            int[] myArray=new int[]{6,3,5,1,2,4,7};
-            int[] tested=test.Sort(myArray);
-            int[] result=new int[]{1,2,3,4,5,6,7};
-            Assert.AreEqual(result,tested);
+           BubbleSorting bubblesort= new BubbleSorting();
+            int[] arraytobesorted=new int[]{6,3,5,1,2,4,7};
+            int[] sortedArray=bubblesort.Sort(arraytobesorted);
+            int[] expectedArray=new int[]{1,2,3,4,5,6,7};
+            Assert.AreEqual(expectedArray,sortedArray);
         }
       }
 }
