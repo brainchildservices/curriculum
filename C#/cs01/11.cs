d3c9120f0b866bb7a19b4aacda0a7e792893fa66
aneeshs005 <aneesh@ebrainchild.services>
2020-09-24 10:47:41 +0530

    Solution 11 completed

---
+++ C#/cs01/11.cs
@@ -0,0 +1,47 @@
+using System;
+namespace Brainchild.Curriculum.Solutions
+{
+// Create a Class with your Name Syamil
+// It  should have internal state to keep firstname,middlename and lastname which should hold your actual names. If Middlename is not there, you can keep it as empty
+// A readonly property should be exposed as FullName  which should return your full name .
+    public class Aneesh
+    {
+        private string firstName="";
+        private string middleName="";
+        private string lastName="";
+        private string fullName;
+        //Read-Only Property
+        public string FullName
+        {
+            get
+            {
+                fullName=firstName+middleName+lastName;
+                return fullName;
+            }
+        }
+        public string FirstName
+        {
+            set
+            {
+                firstName=value;
+            }
+        }
+        public string MiddleName
+        {
+            set
+            {
+                if(value==null)
+                middleName=null;
+                else
+                middleName=" "+value;
+            }
+        }
+        public string LastName
+        {
+            set
+            {
+                lastName=" "+value;
+            }
+        }
+    }
+}
