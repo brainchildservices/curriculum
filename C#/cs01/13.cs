e06966c8059c129e99a596fefed79434aa4ae982
sreeragps <sreerag@brainchild.services>
2020-10-01 17:52:22 +0530

    Added test for pop up value and code aligned.

--- C#/cs01/13.cs
+++ C#/cs01/13.cs
@@ -1,47 +1,47 @@
-using System; 
+using System;
 
 namespace Brainchild.Curriculum.Solutions
-{ 
-	internal class Stack<T>
-	{ 
-		static readonly int MAX = 1000; 
-		int top=-1; 
-		T[] stack = new T[MAX]; 
+{
+    internal class Stack<T>
+    {
+        static readonly int MAX = 1000;
+        int top = -1;
+        T[] stack = new T[MAX];
 
-		internal bool Push(T data) 
-		{ 
-			if (top >= MAX) 
-			{ 
-				Console.WriteLine("Stack Overflow"); 
-				return false; 
-			} 
-			else
-			{ 
-				stack[++top] = data; 
-				return true; 
-			} 
-		} 
+        internal bool Push(T data)
+        {
+            if (top >= MAX)
+            {
+                Console.WriteLine("Stack Overflow");
+                return false;
+            }
+            else
+            {
+                stack[++top] = data;
+                return true;
+            }
+        }
 
-		internal T Pop() 
-		{ 
-			if (top < 0) 
-			{ 
-				Console.WriteLine("Stack Underflow"); 
-				return default(T);
-			} 
-			else
-			{ 
-				T value = stack[top--]; 
-                stack[top+1]=default(T);
-				return value; 
-			} 
-		} 
+        internal T Pop()
+        {
+            if (top < 0)
+            {
+                Console.WriteLine("Stack Underflow");
+                return default(T);
+            }
+            else
+            {
+                T value = stack[top--];
+                stack[top + 1] = default(T);
+                return value;
+            }
+        }
 
-		internal T[] Output() 
-		{ 		
+        internal T[] Output()
+        {
             return stack;
-		} 
-	} 
+        }
+    }
 
 
-} 
+}
--- C#/cs01/tests/13.cs
+++ C#/cs01/tests/13.cs
@@ -18,12 +18,13 @@ namespace Brainchild.Curriculum
             stringstack.Push("abc");
             stringstack.Push("abcd");
             stringstack.Push("abcde");
-            stringstack.Pop();
+            string popped = stringstack.Pop();
             string[] actual = stringstack.Output();
             string[] expecting = new string[1000];
             expecting[0] = "abc";
             expecting[1] = "abcd";
             Assert.AreEqual(actual, expecting);
+            Assert.AreEqual(popped, "abcde");
         }
         [Test]
         public void CheckingGenericClassIntStack()
@@ -32,12 +33,14 @@ namespace Brainchild.Curriculum
             intstack.Push(5);
             intstack.Push(7);
             intstack.Push(88);
-            intstack.Pop();
+            int popped = intstack.Pop();
             int[] actual = intstack.Output();
             int[] expecting = new int[1000];
             expecting[0] = 5;
             expecting[1] = 7;
             Assert.AreEqual(actual, expecting);
+            Assert.AreEqual(popped, 88);
+
         }
         [Test]
         public void CheckingGenericClassDoubleStack()
@@ -46,12 +49,13 @@ namespace Brainchild.Curriculum
             doublestack.Push(5.8);
             doublestack.Push(9.6);
             doublestack.Push(88.55);
-            doublestack.Pop();
+            double popped = doublestack.Pop();
             double[] actual = doublestack.Output();
             double[] expecting = new double[1000];
             expecting[0] = 5.8;
             expecting[1] = 9.6;
             Assert.AreEqual(actual, expecting);
+            Assert.AreEqual(popped, 88.55);
         }
 
 
