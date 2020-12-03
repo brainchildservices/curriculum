84716cdd4a11c0d14d5cf5bb6c81494f654aee27
VishnuKulakkattu <Vishnu.kulakkattu@gmail.com>
2020-12-03 17:32:19 +0530

    Bubble sort Completed

--- C#/cs01/03.cs
+++ C#/cs01/03.cs
@@ -4,8 +4,24 @@ namespace Brainchild.Curriculum.Solutions
     //implement bubblesort in C# and also create the necessary tests
     public class BubbleSort
     {
-        public int[] Sort(int[] arr){
-            throw new NotImplementedException();
+        public int[] Sort(int[] arr)
+        {
+
+            int n =arr.Length;
+            int temp;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+            return arr;
         }
     }
 }
