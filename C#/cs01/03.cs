a72017c4bdf8ec004139b8827129629d10776fa9
KnightZzz999 <78071547+KnightZzz999@users.noreply.github.com>
2021-04-07 12:41:25 +0530

    completed question 3

--- C#/cs01/03.cs
+++ C#/cs01/03.cs
@@ -4,8 +4,23 @@ namespace Brainchild.Curriculum.Solutions
     //implement bubblesort in C# and also create the necessary tests
     public class BubbleSort
     {
-        public int[] Sort(int[] arr){
-            throw new NotImplementedException();
+        public int[] Sort(int[] arr)
+        {
+		    int temp;
+		    for (int i = 0; i < arr.Length; i++)
+		    {
+			   for (int j =0; j < arr.Length-1; j++)
+			    {
+				   if (arr[j+1] < arr[j])
+				    {
+				    	temp = arr[j];
+					    arr[j] = arr[j + 1];
+					    arr[j + 1] = temp;
+				    }
+			    }
+		    }
+            return arr;
         }
+        
     }
 }
