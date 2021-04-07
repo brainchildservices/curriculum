44cfd87a28e28ee31e0fb66341b967d2b5588f4d
KnightZzz999 <78071547+KnightZzz999@users.noreply.github.com>
2021-04-07 13:34:15 +0530

    complete Q3

--- C#/cs01/03.cs
+++ C#/cs01/03.cs
@@ -7,19 +7,19 @@ namespace Brainchild.Curriculum.Solutions
        public int[] Sort(int[] arr)
        {
 		    int temp;
-		      for (int i=0;i<arr.Length;i++)
-		       {
-			       for (int j=0;j<arr.Length-1;j++)
-			       {
-				       if (arr[j+1]<arr[j])
-				       {
-				            	temp=arr[j];
-					            arr[j]=arr[j+1];
-					            arr[j+1]=temp;
-				        }
-			        }
-		        }
-                                return arr;
+		    for (int i=0;i<arr.Length;i++)
+		    {
+			    for (int j=0;j<arr.Length-1;j++)
+			    {
+				    if (arr[j+1]<arr[j])
+				    {
+				        temp=arr[j];
+					    arr[j]=arr[j+1];
+					    arr[j+1]=temp;
+				    }
+			    }
+		    }
+        return arr;
         }     
     }
 }
